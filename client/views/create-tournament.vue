86909e0c43a9383f9d3efcdca9a0f012d9fcf199
Tony Bradley <antbradley91@gmail.com>
2017-03-21 22:12:54 -0400

    editorconfig and trimtrailing

--- client/views/create-tournament.vue
+++ client/views/create-tournament.vue
@@ -1,15 +1,40 @@
 <template>
 <div>
   <v-navbar></v-navbar>
-  <div class='center measure ph2'>
-    <h3>Create Tournament</h3>
+  <div class='center measure ph2 pt3'>
+    <div :class='styles.qaGroup'>
+      <div :class='styles.question'>
+        What is the name of the tournament?
+      </div>
+      <div :class='styles.answer'>
+
+      </div>
+    </div>
+    <div :class='styles.qaGroup'>
+      <div :class='styles.question'>
+        What bracket type?
+      </div>
+      <div :class='styles.answer'>
+
+      </div>
+    </div>
+    <div :class='styles.qaGroup'>
+      <div :class='styles.question'>
+        Private, Public, or Registration?
+      </div>
+      <div :class='styles.answer'>
+
+      </div>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
 const styles = {
-
+  qaGroup: '',
+  question: '',
+  answer: ''
 }
 
 exports.methods = {
